0932c73e30246218de48c28a2d805c0d01efeef5
shawn-exe <112694633+shawn-exe@users.noreply.github.com>
2024-03-18 18:27:17 +0530

    TensorFlow model Added

--- src/components/mainmodule.tsx
+++ src/components/mainmodule.tsx
@@ -1,13 +1,46 @@
 'use client'
-import { Button } from './ui/button'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam'
-type Props = {}
+import * as cocossd from '@tensorflow-models/coco-ssd';
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import { DetectedObject, ObjectDetection } from '@tensorflow-models/coco-ssd';
 
+type Props = {}
+let interval: any =null;
 const MainModule = (props: Props) => {
     const webcamRef = useRef<Webcam>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-   // const [mirrored,setMirrored]=useState<boolean>(false);
+    const [model,setModel]=useState<ObjectDetection>();
+    useEffect(()=>
+    {
+      initModel();
+    },[])
+
+    async function initModel() {
+      const loadedModel: ObjectDetection = await cocossd.load({base:'mobilenet_v2'});
+      setModel(loadedModel);
+    }
+
+  async function runPrediction()
+  {
+    if(model && webcamRef.current && webcamRef.current.video && webcamRef.current.video.readyState === 4)
+    {
+      const predictions: DetectedObject[] = await model.detect(webcamRef.current.video);
+      console.log(predictions);
+    }
+  }
+
+  useEffect(()=>
+    {
+      interval=setInterval(()=>{
+        runPrediction();
+      },100)
+      return()=>clearInterval(interval);//To clear the previous intervals.. incase of reload
+    },[webcamRef.current,model])//Mirrored state is not added here
+  
+   
+  
   return (
     <div className='w-full flex flex-col items-center gap-2'>
         <div className='relative'> 
@@ -16,14 +49,6 @@ const MainModule = (props: Props) => {
             <canvas ref={canvasRef} className='h-full w-full absolute top-0 left-0 object-contain' ></canvas>
             </div>
         </div>
-        <div className='w-[400px] py-2 flex flex-row justify-center space-x-10 mb-2 border-secondary/5 border-2 shadow-md rounded-md'>
-             <Button>abcd</Button>
-             <Button>abcd</Button>
-             <Button>abcd</Button>
-        </div>
-        
-
-
     </div>
   )
 }
