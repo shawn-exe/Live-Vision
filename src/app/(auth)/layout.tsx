468dd27b3773e171fbb3ac7df0769a58f63ecde4
shawn-exe <112694633+shawn-exe@users.noreply.github.com>
2024-03-26 19:43:22 +0530

    auth layout fixed

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import {FC,ReactNode} from 'react';
-
+import Link from 'next/link';
+import { ArrowLeft, ArrowUpLeftSquareIcon } from 'lucide-react';
 interface AuthlayoutProps{//This is to remove the type warning of next js;
     children:ReactNode;
 }
@@ -7,6 +8,16 @@ interface AuthlayoutProps{//This is to remove the type warning of next js;
 const Authlayout: FC<AuthlayoutProps> = ({children}) => {
   return (
     <div className='w-full h-full flex flex-col justify-center items-center'>
+       <Link href={'/'}>
+         <div className='p-10 top-0 left-0 fixed flex flex-row gap-1 justify-center'>
+            <div className=' text-white'>
+             <ArrowUpLeftSquareIcon/>
+            </div>
+            <div className=' text-white'>
+             Go back
+            </div>
+          </div>
+       </Link>
     <div className='bg-slate-200 p-10 w-1/3 rounded-md '>
       {children}
     </div>
